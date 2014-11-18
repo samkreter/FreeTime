54ed2a2f6b7c087aab631ef83b0ba6e976eb5100
Kyle McCarthy <kjmd54@mail.missouri.edu>
2014-11-17 21:00:48 -0600

    inverse relationships

--- app/models/ClassM.php
+++ app/models/ClassM.php
@@ -7,4 +7,12 @@ class ClassM extends Eloquent
     public $timestamps = true;
     protected $fillable = array('title', 'instructor', 'start', 'end');
 
+    /**
+     * Get all the schedules the class appears on
+     * @return mixed
+     */
+    public function schedules()
+    {
+        return $this->belongsToMany('Schedule', 'schedule_classes', 'schedule_id', 'class_id');
+    }
 }
--- app/models/Sport.php
+++ app/models/Sport.php
@@ -7,4 +7,13 @@ class Sport extends Eloquent
     public $timestamps = true;
     protected $fillable = array('name', 'start', 'end', 'min_players', 'max_players');
 
+    /**
+     * Get all the schedules the sport appears on
+     * @return mixed
+     */
+    public function schedules()
+    {
+        return $this->belongsToMany('Schedule', 'schedule_sports');
+    }
+
 }
